4db525b7c07ba024103bf3185e441f070cdfaeb0
Alex Kramer <alex@keicreations.nl>
2021-06-30 00:41:40 +0200

    add exceptions to adhere by vuex convention

--- index.js
+++ index.js
@@ -28,7 +28,25 @@ module.exports = {
             singleQuote: true,
             tabWidth: 4,
         }],
-        "vue/html-indent": ["error", 4]
+        "vue/html-indent": ["error", 4],
+        "no-shadow": ["error", { "allow": ["state"] }],
+        "no-param-reassign": [
+            "error",
+            {
+                "props": true,
+                "ignorePropertyModificationsFor": [ // All properties except state are in the ignorePropertyModificationsFor array by default.
+                    "state",
+                    "acc",
+                    "e",
+                    "ctx",
+                    "req",
+                    "request",
+                    "res",
+                    "response",
+                    "$scope"
+                ]
+            }
+        ],
     },
     reportUnusedDisableDirectives: true,
     ignorePatterns: [
